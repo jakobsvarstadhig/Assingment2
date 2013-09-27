b8ab8a0493aff4d74138f57af3bb755848685812
Jakob Sand Svarstad <watashiwaore@gmail.com>
2013-09-27 15:07:16 +0200

    added nowegian language xml

--- gen/com/example/assingment2/R.java
+++ gen/com/example/assingment2/R.java
@@ -33,10 +33,10 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int BarometerButton=0x7f050002;
-        public static final int BarometerText=0x7f050003;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int sensor_list_label=0x7f050003;
+        public static final int sensor_return_text=0x7f050002;
     }
     public static final class style {
         /** 
