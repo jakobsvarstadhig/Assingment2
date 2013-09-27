df1ccbc90ee0f2c3324f13d935174ffd40bd57fa
Jakob Sand Svarstad <watashiwaore@gmail.com>
2013-09-27 11:00:22 +0200

    added unrefined barometer
    we have to put this into a button to use other sensors the same way

--- src/com/example/assingment2/MainActivity.java
+++ src/com/example/assingment2/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.assingment2;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.Service;
 import android.view.Menu;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SensorEventListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		
+		// Look for barometer sensor
+		SensorManager snsMgr = (SensorManager) getSystemService(Service.SENSOR_SERVICE);
+		Sensor pS = snsMgr.getDefaultSensor(Sensor.TYPE_PRESSURE);
+		snsMgr.registerListener(this, pS, SensorManager.SENSOR_DELAY_UI);
+
+		
 		setContentView(R.layout.activity_main);
 	}
 
@@ -19,4 +33,14 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	
+	@Override
+	    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+	    }
+	    @Override
+	    public void onSensorChanged(SensorEvent event) {
+	        float[] values = event.values;
+	        Toast toast = Toast.makeText(getBaseContext(),"It is " + values[0],10);
+	        toast.show();
+	    }
 }
