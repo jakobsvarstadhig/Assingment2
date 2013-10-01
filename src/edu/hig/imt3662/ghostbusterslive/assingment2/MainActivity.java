e26677ddfa2731be696072e3e7be295cc51de44a
PK <pekaaw@gmail.com>
2013-10-01 23:28:20 +0200

    Commented the code and deleted some unused code.

--- src/edu/hig/imt3662/ghostbusterslive/assingment2/MainActivity.java
+++ src/edu/hig/imt3662/ghostbusterslive/assingment2/MainActivity.java
@@ -28,38 +28,43 @@ public class MainActivity extends ListActivity implements SensorEventListener {
 		super.onCreate(savedInstanceState);
 		
 		
-		// Look for barometer sensor
+		// Get a sensormanager and make it create a list of the sensors on a device
 		mSensorManager = (SensorManager) getSystemService(Service.SENSOR_SERVICE);
 		mSensors = mSensorManager.getSensorList( Sensor.TYPE_ALL );
-//		Sensor pS = snsMgr.getDefaultSensor(Sensor.TYPE_PRESSURE);
-//		snsMgr.registerListener(this, pS, SensorManager.SENSOR_DELAY_UI);
 
+		// Create a list like the sensor-list, but this one will contain the sensornames
 		mSensorNames = new ArrayList<String>();
 		for( int i = 0; i < mSensors.size(); ++i ) {
 			mSensorNames.add( mSensors.get( i ).getName() );
 		}
 		
+		// Set the adapter for this listActivity to show a list of sensornames
 		mMainviewListAdapter = new ArrayAdapter<String>( this,
 				android.R.layout.simple_list_item_1,
 				mSensorNames );
 		
+		// Activate the adapter - this will show the list in the activity
 		setListAdapter( mMainviewListAdapter );
-		//setContentView(R.layout.activity_main);
-		
 	}
 	
+	/**
+	 * Clicking on a listEntry
+	 * 
+	 * By clicking on a list-entry, the corresponding sensor will activate and start
+	 * communicating its output-info. The action happens when the values changes and
+	 * the method called onSensorChanged() are fired up.
+	 */
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 		super.onListItemClick(l, v, position, id);
-//		Toast.makeText(
-//				getBaseContext(), 
-//				getString(R.string.toast_message_p1) + mSensors.get(position).getPower() + getString(R.string.toast_message_p2), 
-////				"Uses " + mSensors.get(position).getPower() + "mA while in use...", 
-//				Toast.LENGTH_SHORT)
-//			.show();
 		mSensorManager.registerListener(this, mSensors.get(position), SensorManager.SENSOR_DELAY_NORMAL);
 	}
 
+	/**
+	 * OptionsMenu
+	 * 
+	 * Inflate the menu
+	 */
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -67,12 +72,29 @@ public class MainActivity extends ListActivity implements SensorEventListener {
 		return true;
 	}
 
-	
+	/**
+	 * onAccuracyChanged()
+	 * 
+	 * When the accuracy of a sensor changes, this will be fired. The method is not
+	 * used in our application, but it kind of fit together with the next method.
+	 */
 	@Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
-		// ToDo: fill here if necessary.
+		// ToDo: fill here if necessary
 	}
 
+	/**
+	 * onSensorChanged()
+	 * 
+	 * This is where the magic happens. A sensor has changed it's value and we are
+	 * notified through an event. When this occurs and our event is executed, we
+	 * find data from the event and toast it.
+	 * <p>
+	 * The result will be a toast message telling the name of the sensor and 
+	 * the output-value from this sensor.
+	 * <p>
+	 * At the end the sensorlistener is unregistered. 
+	 */
 	@Override
     public void onSensorChanged(SensorEvent event) {
         float[] values = event.values;
